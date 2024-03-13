0d3ed55dc8d80fe3f4efd0d4fa85d8ad2234b878
raaj-oo7 <raj.makadia22@gmail.com>
2024-03-13 17:32:24 +0530

    update files and app js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header />  
-    
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
       <Footer />
     </div>
   );
